ba9b5a1a8e81d34725686398c7401a566fbdf4ee
Oleksii Klymenko <alex.klimenko@gmail.com>
2023-02-25 20:20:47 +0200

    use createAsyncThunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import todoSlice from "../features/todo/todoSlice";
+import postSlice from "../features/post/postSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        todo: todoSlice
+        todo: todoSlice,
+        post: postSlice
     },
 })
